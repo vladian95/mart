8ca9bb7fafc24b16d7a32605cd08dd3b0f015e3c
vladian95 <kvankvan1995@gmail.com>
2021-05-17 14:08:21 +0200

    added icons and user in header

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,20 @@
 $(function(){
 
-    $('.followers__inner').slick();
+    var mixer = mixitup('.newest__items-inner');
+
+    $('.followers__items-inner').slick({
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        prevArrow: '<button class="slick-arrow slick-prev"><img src="images/icons/arrow-left.png" alt="#"></button>',
+        nextArrow: '<button class="slick-arrow slick-next"><img src="images/icons/arrow-right.png" alt="#"></button>',
+    });
+
+    // $('.weekly__content-inner').slick({
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1,
+    //     prevArrow: '<button class="slick-arrow slick-prev"><img src="images/icons/arrow-left.png" alt="#"></button>',
+    //     nextArrow: '<button class="slick-arrow slick-next"><img src="images/icons/arrow-right.png" alt="#"></button>',
+    // });
   
 });
 
