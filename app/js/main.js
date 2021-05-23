57e9c7a06593a5e383bab1bfbecccef17b0244fa
vladian95 <kvankvan1995@gmail.com>
2021-05-23 23:26:05 +0200

    added sliderbar but dont finished

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,19 @@
 $(function(){
 
-    var mixer = mixitup('.newest__items-inner');
-
     $('.followers__items-inner').slick({
         slidesToShow: 3,
         slidesToScroll: 3,
         prevArrow: '<button class="slick-arrow slick-prev"><img src="images/icons/arrow-left.png" alt="#"></button>',
         nextArrow: '<button class="slick-arrow slick-next"><img src="images/icons/arrow-right.png" alt="#"></button>',
+    });
+
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 400,
+        from: 30,
+        to: 300,
+        prefix: "$"
     });
 
     // $('.weekly__content-inner').slick({
@@ -15,6 +22,8 @@ $(function(){
     //     prevArrow: '<button class="slick-arrow slick-prev"><img src="images/icons/arrow-left.png" alt="#"></button>',
     //     nextArrow: '<button class="slick-arrow slick-next"><img src="images/icons/arrow-right.png" alt="#"></button>',
     // });
+
+    var mixer = mixitup('.newest__items-inner');
   
 });
 
